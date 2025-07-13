e121697b5cab505d55fe391b36519ce62a26cb71
atman-33 <gpbjk0304@gmail.com>
2025-07-13 22:33:08 +0900

    feat: install zustand and update db schema

--- database/schema.ts
+++ database/schema.ts
@@ -1,5 +1,16 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-import { account, session, user, verification } from './auth-schema';
+import { account, session, user, verification } from "./auth-schema";
+
+const mangaLogs = sqliteTable("manga_logs", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  title: text("title").notNull(),
+  score: integer("score"),
+  status: text("status"),
+  volume_progress: integer("volume_progress"),
+  chapter_progress: integer("chapter_progress"),
+  note: text("note"),
+  user_id: text("user_id").references(() => user.id),
+});
 
 const guestBook = sqliteTable("guestBook", {
   id: integer().primaryKey({ autoIncrement: true }),
@@ -7,6 +18,13 @@ const guestBook = sqliteTable("guestBook", {
   email: text().notNull().unique(),
 });
 
-export const schema = { user, session, account, verification, guestBook };
+export const schema = {
+  user,
+  session,
+  account,
+  verification,
+  guestBook,
+  mangaLogs,
+};
 
-export { account, guestBook, session, user, verification };
+export { account, guestBook, mangaLogs, session, user, verification };
